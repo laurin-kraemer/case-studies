6e62660b3cca32ca8c64a08b99afea30fac16cba
laurin-kraemer <65118747+laurin-kraemer@users.noreply.github.com>
2021-11-07 20:04:47 -0800

    Cleaning
    Replaced manual cleaning in data explorer with importing clean csv

--- Air France/data-explorer/ui.R
+++ Air France/data-explorer/ui.R
@@ -12,39 +12,8 @@ library(readxl)
 library(tidyverse)
 
 # Import data
-kayak <- read_excel("vSpreadsheet_Data.xls", 
-                    sheet = "Kayak")
-
-doubleclick <- read_excel("Spreadsheet_Data.xls", 
-                          sheet = "DoubleClick")
-
-#Convert to dataframe
-doubleclick <- as.data.frame(doubleclick)
-
-#Look for weird stuff
-table(doubleclick$`Match Type`)
-
-# The NAs have to be removed.
-doubleclick_clean <- na.omit(doubleclick)
-
-# Notice how the number of rows gets reduced 
-print(nrow(doubleclick_clean))
-
-# Look for Spelling mistakes
-unique(doubleclick_clean $`Bid Strategy`)
-
-# Replace Typos
-doubleclick_clean$`Bid Strategy` <- gsub("Postiion 1-4 Bid Strategy","Position 1-4 Bid Strategy",doubleclick_clean$`Bid Strategy`)
-
-doubleclick_clean$`Bid Strategy` <- gsub("Position 1 -2 Target","Position 1-2 Target",doubleclick_clean$`Bid Strategy`)
-
-# Create data set for analysis
-sem <- doubleclick_clean[,c('Campaign','Keyword','Keyword Group','Publisher Name', 'Bid Strategy','Engine Click Thru %','Match Type','Trans. Conv. %','Total Cost/ Trans.','Impressions')]
-
-
-
-
-
+library(readr)
+data_massaged <- read_csv("data-explorer/data_massaged.csv")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
